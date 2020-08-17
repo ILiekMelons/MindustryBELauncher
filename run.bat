b10df58e58b799fe320d75399ea8c82cfd3c71aa
ILiekMelons <49169907+ILiekMelons@users.noreply.github.com>
2020-08-17 05:40:06 +0100

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
 @echo off
+move main.py %USERPROFILE%\BEWrapper 2> nul
+cd %USERPROFILE%\BEWrapper
+python3 -m venv mindustry-env
+.\mindustry-env\Scripts\activate.bat
 pip3 install requests wget pathlib
 python3 main.py
