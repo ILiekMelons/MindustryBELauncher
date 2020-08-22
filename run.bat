9a42c6b863467f565e4c9349eeb3ff95f6b4d75c
ILiekMelons <49169907+ILiekMelons@users.noreply.github.com>
2020-08-22 15:56:46 +0100

    Delete run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-move main.py %USERPROFILE%\BEWrapper 2> nul
-cd %USERPROFILE%\BEWrapper
-python3 -m venv mindustry-env
-.\mindustry-env\Scripts\activate.bat
-pip3 install requests wget pathlib
-python3 main.py
