ec8f0ffad951940cc6aae95108006eca1b4df099
ILiekMelons <49169907+ILiekMelons@users.noreply.github.com>
2020-08-22 15:56:36 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mv main.py ~/BEWrapper 2> /dev/null
-cd ~/BEWrapper
-python3 -m venv mindustry-env
-source mindustry-env/bin/activate
-pip3 install requests wget pathlib
-python3 main.py
